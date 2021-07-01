277a0d675d339cbfca71098712d167c5c6fd0ddd
julio cabrera <juliocabrera820@gmail.com>
2021-07-01 02:34:11 -0500

    set routes
    add admin routes
    add employee routes
    add authentication routes

--- reloj-checador/config/routes.rb
+++ reloj-checador/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_user
+    end
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_user
+    end
+  end
+  resources :employees
+  resources :admin
 end
