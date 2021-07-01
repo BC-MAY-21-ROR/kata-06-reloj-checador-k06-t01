ee5df4b3b9629640da1dacd334b39c790318d6e6
julio cabrera <juliocabrera820@gmail.com>
2021-07-01 02:36:14 -0500

    redirect after signing in

--- reloj-checador/app/controllers/application_controller.rb
+++ reloj-checador/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    if current_user.employee?
+      employee_url(current_user.id)
+    elsif current_user.admin?
+      admin_url(current_user.id)
+    end
+  end
 end
