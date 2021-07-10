6134b3bf500bead82df81518ebab14022257d859
julio cabrera <juliocabrera820@gmail.com>
2021-07-09 22:38:01 -0500

    add set_company method and company_params

--- reloj-checador/app/controllers/companies_controller.rb
+++ reloj-checador/app/controllers/companies_controller.rb
@@ -1,30 +1,47 @@
 class CompaniesController < ApplicationController
-    def index
-        @companies = Company.all 
-        @company = Company.new
-    end
+  before_action :set_company, only: %i[show edit update destroy]
 
-    def show; end
-    def edit; end
-    def new; end
+  def index
+    @companies = Company.all
+    @company = Company.new
+  end
 
-    def create
-        company = Company.create(params.require(:company).permit(:name, :address))
-        flash[:notice] = 'Comapany created'
-        redirect_to action: "index"
-    end
+  def show; end
 
-    def update
-        company = Company.find(params[:id])
-        company.update(params.require(:company).permit(:name, :address))
-        flash[:notice] = 'Comapany updated'
-        redirect_to action: "index"
-    end
+  def edit; end
 
-    def destroy
-        company = Company.find(params[:id])
-        company.destroy();
-        flash[:notice] = 'Comapany deleted'
-        redirect_to action: "index"
+  def new; end
+
+  def create
+    @company = Company.create(company_params)
+    if @company.save
+      flash[:success] = 'company successfully created'
+      redirect_to companies_path
+    else
+      flash[:warning] = 'company was not successfully created'
+      redirect_to companies_path
     end
+  end
+
+  def update
+    @company.update(company_params)
+    flash[:success] = 'Company was updated'
+    redirect_to companies_path
+  end
+
+  def destroy
+    @company.destroy
+    flash[:danger] = 'Company was deleted'
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :address)
+  end
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
 end
